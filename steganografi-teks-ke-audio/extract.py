4b264fd01cbaa88af27589ad71dbe6bcd7d2a77a
GentaHI <58261801+GentaHI@users.noreply.github.com>
2019-11-27 23:42:45 +0700

    extract.py
    nih terakhiran

--- steganografi-teks-ke-audio/extract.py
+++ steganografi-teks-ke-audio/extract.py
@@ -6,3 +6,14 @@ frame_bytes = bytearray(list(audio.readframes(audio.getnframes())))
 
 # Mengekstrak LSB dari setiap byte
 extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+
+# Kembali konversikan LSB ke string pesan
+pesan = "".join(chr(
+    int("".join(map(str, extracted[i:i+8])), 2)) for i in range(0, len(extracted), 8))
+
+# Menghilangkan karakter padding
+ekstraksi = pesan.split("###")[0]
+
+# Menampilkan string hasil ekstraksi
+print("Ekstraksi berhasil: "+ekstraksi)
+audio.close()
